08b40fb4d623c27b7befb318731b675a7e4ed182
Angel <135652587+Angel2619-k@users.noreply.github.com>
2026-03-08 01:14:56 +0530

    Add initials printing in initials.py

---
+++ initials.py
@@ -0,0 +1,8 @@
+# My intials
+print('       A                R R R R      ')
+print('     A   A              R     R     ')
+print('    A      A            R     R  ')
+print('   A AAAAAA A           R R R R   ')
+print('  A          A          R R   ')
+print(' A            A         R  R')
+print('A              A        R   R')
