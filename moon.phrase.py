6133691c4a2bea4af0ce14741722141cf397fd03
Angel <135652587+Angel2619-k@users.noreply.github.com>
2026-03-11 01:06:27 +0530

    Implement moon_phase function for lunar phases

---
+++ moon.phrase.py
@@ -0,0 +1,23 @@
+#Moon Phrases
+def moon_phase(phase):
+    if phase == 'New Moon':
+      return'🌑'
+    elif phase == 'Waxing Crescent' :
+      return'🌒'
+    elif phase == 'First Quarter'  :
+      return'🌓'
+    elif phase == 'Waxing Gibbous'  :
+      return'🌔'
+    elif phase == 'Full Moon' :
+      return'🌕'
+    elif phase == 'Waning Gibbous'  :
+      return'🌖'
+    elif phase == 'Last Quarter':
+      return'🌗'
+    elif phase == 'Waning Crescent'   :
+      return '🌘'
+    else :
+      return'invalid'
+
+answer = moon_phase('New Moon')
+print (answer)
