98ca9acf9bec75fe38f31f85b58687ab340fc115
Angel <135652587+Angel2619-k@users.noreply.github.com>
2026-03-11 01:02:19 +0530

    Implement basic arithmetic operations in calculator.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+#calculator
+#adding two numbers
+def add (a,b):
+  return a+b 
+print(add(3,5)) 
+#subtracting two numbers
+def subtract (a,b):
+  return a-b
+print(subtract(8,5))
+#multiplying two numbers
+def multiply(a,b):
+  return a*b
+print(multiply(4,5))
+#dividing two  number
+def divide(a,b):
+  return a/b
+print(divide(8,2))
+#expontial numbers
+def exp(a,b):
+  return a**b
+print(exp(5,2))
